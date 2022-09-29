0411952ab2cf7818cdb33a73e2b1e0e3078baf45
jogogi7613 <jogogi7613@migonom.com>
2022-09-29 17:32:43 -0400

    Урок 4. создания миграции

---
+++ database/migrations/2022_09_29_183400_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('images')->nulable();
+            $table->text('content');
+            $table->unsignedBigInteger('likes');
+            $table->boolean('is_published')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
