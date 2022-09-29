3ff55ad82f5dee3662f0e9ee063eca80843197ce
jogogi7613 <jogogi7613@migonom.com>
2022-09-29 09:52:42 -0400

    урок 2, первый route в laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Функция принимает два параметра первый url, второй анонимная функция
+ * @return strung
+ *
+*/
+Route::get('/my_page',function(){
+    return 'text';
+});
