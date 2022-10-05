a993d715addc4b8c66003b587650bfcc89d0c740
jogogi7613 <jogogi7613@migonom.com>
2022-10-05 13:23:02 -0400

    Урок 13. Добовления столбца в таблицу в базе, например если мы хотим добавить столбец в таблицу posts создаем нову миграцию add_column_to_posts_table, to_posts_table , указывает на уже существующую миграцию create_posts_table

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -504,7 +504,7 @@ class ComposerStaticInitc4bc358afb50b8373408f6a8a704ded4
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
-        'App\\Http\\Controllers\\MyFirstController' => __DIR__ . '/../..' . '/app/Http/Controllers/MyFirstController.php',
+        'App\\Http\\Controllers\\PostController' => __DIR__ . '/../..' . '/app/Http/Controllers/PostController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
