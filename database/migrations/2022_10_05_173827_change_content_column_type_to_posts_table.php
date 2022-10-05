36609663e6608708d237eb56b949a0f2726f0809
jogogi7613 <jogogi7613@migonom.com>
2022-10-05 13:48:51 -0400

    Урок 13. изменяем тип колонки content с text на string

---
+++ database/migrations/2022_10_05_173827_change_content_column_type_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeContentColumnTypeToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('post_content')->change();//изменяем тип колонки content с text на string, post_content потому,что
+            //предидущем коммите меняли content на post_content
+            //и когда вызываем php artisan migrate, content меняется на post_content
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('post_content')->change();// обратно откатываем тип на text
+        });
+    }
+}
